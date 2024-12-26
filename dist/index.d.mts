4b287e4c36375d64f49c49e6af12eb0d0a1e1808
Babyze <danghuynhanh1999@gmail.com>
2024-12-26 20:22:45 +0700

    Update build

--- dist/index.d.mts
+++ dist/index.d.mts
@@ -1,3 +1,7 @@
+import * as _nestjs_common from '@nestjs/common';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+import { Repository, DeepPartial, FindOneOptions, FindManyOptions } from 'typeorm';
+
 interface ITimestamp {
     second: number;
 }
@@ -10,4 +14,28 @@ declare class Timestamp implements ITimestamp {
 declare class Timestamp2 {
 }
 
-export { Timestamp, Timestamp2 };
+declare class DatabaseModule {
+    static forFeature(models: EntityClassOrSchema[]): _nestjs_common.DynamicModule;
+}
+
+interface DatabaseInterfaceRepository<T> extends Repository<T> {
+    createAndSave(data: DeepPartial<T>): Promise<T>;
+    createManyAndSave(data: DeepPartial<T>[]): Promise<T[]>;
+    findOneByCondition(filterCondition: FindOneOptions<T>): Promise<T>;
+    findAll(options?: FindManyOptions<T>): Promise<T[]>;
+    findWithRelations(relations: FindManyOptions<T>): Promise<T[]>;
+    findOne(options: FindOneOptions<T>): Promise<T>;
+}
+
+declare abstract class BaseAbstractRepository<T> extends Repository<T> implements DatabaseInterfaceRepository<T> {
+    private readonly repo;
+    constructor(repo: Repository<T>);
+    createAndSave(data: DeepPartial<T>): Promise<T>;
+    createManyAndSave(data: DeepPartial<T>[]): Promise<T[]>;
+    findOneByCondition(filterCondition: FindOneOptions<T>): Promise<T>;
+    findAll(options?: FindManyOptions<T>): Promise<T[]>;
+    findWithRelations(relations: FindManyOptions<T>): Promise<T[]>;
+    findOne(options: FindOneOptions<T>): Promise<T>;
+}
+
+export { BaseAbstractRepository, DatabaseModule, Timestamp, Timestamp2 };
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,15 @@
 "use strict";
+var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __knownSymbol = (name, symbol) => (symbol = Symbol[name]) ? symbol : Symbol.for("Symbol." + name);
+var __typeError = (msg) => {
+  throw TypeError(msg);
+};
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
@@ -16,10 +23,50 @@ var __copyProps = (to, from, except, desc) => {
   return to;
 };
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var __decoratorStart = (base) => [, , , __create(base?.[__knownSymbol("metadata")] ?? null)];
+var __decoratorStrings = ["class", "method", "getter", "setter", "accessor", "field", "value", "get", "set"];
+var __expectFn = (fn) => fn !== void 0 && typeof fn !== "function" ? __typeError("Function expected") : fn;
+var __decoratorContext = (kind, name, done, metadata, fns) => ({ kind: __decoratorStrings[kind], name, metadata, addInitializer: (fn) => done._ ? __typeError("Already initialized") : fns.push(__expectFn(fn || null)) });
+var __decoratorMetadata = (array, target) => __defNormalProp(target, __knownSymbol("metadata"), array[3]);
+var __runInitializers = (array, flags, self, value) => {
+  for (var i = 0, fns = array[flags >> 1], n = fns && fns.length; i < n; i++) flags & 1 ? fns[i].call(self) : value = fns[i].call(self, value);
+  return value;
+};
+var __decorateElement = (array, flags, name, decorators, target, extra) => {
+  var fn, it, done, ctx, access, k = flags & 7, s = !!(flags & 8), p = !!(flags & 16);
+  var j = k > 3 ? array.length + 1 : k ? s ? 1 : 2 : 0, key = __decoratorStrings[k + 5];
+  var initializers = k > 3 && (array[j - 1] = []), extraInitializers = array[j] || (array[j] = []);
+  var desc = k && (!p && !s && (target = target.prototype), k < 5 && (k > 3 || !p) && __getOwnPropDesc(k < 4 ? target : { get [name]() {
+    return __privateGet(this, extra);
+  }, set [name](x) {
+    return __privateSet(this, extra, x);
+  } }, name));
+  k ? p && k < 4 && __name(extra, (k > 2 ? "set " : k > 1 ? "get " : "") + name) : __name(target, name);
+  for (var i = decorators.length - 1; i >= 0; i--) {
+    ctx = __decoratorContext(k, name, done = {}, array[3], extraInitializers);
+    if (k) {
+      ctx.static = s, ctx.private = p, access = ctx.access = { has: p ? (x) => __privateIn(target, x) : (x) => name in x };
+      if (k ^ 3) access.get = p ? (x) => (k ^ 1 ? __privateGet : __privateMethod)(x, target, k ^ 4 ? extra : desc.get) : (x) => x[name];
+      if (k > 2) access.set = p ? (x, y) => __privateSet(x, target, y, k ^ 4 ? extra : desc.set) : (x, y) => x[name] = y;
+    }
+    it = (0, decorators[i])(k ? k < 4 ? p ? extra : desc[key] : k > 4 ? void 0 : { get: desc.get, set: desc.set } : target, ctx), done._ = 1;
+    if (k ^ 4 || it === void 0) __expectFn(it) && (k > 4 ? initializers.unshift(it) : k ? p ? extra = it : desc[key] = it : target = it);
+    else if (typeof it !== "object" || it === null) __typeError("Object expected");
+    else __expectFn(fn = it.get) && (desc.get = fn), __expectFn(fn = it.set) && (desc.set = fn), __expectFn(fn = it.init) && initializers.unshift(fn);
+  }
+  return k || __decoratorMetadata(array, target), desc && __defProp(target, name, desc), p ? k ^ 4 ? extra : desc : target;
+};
+var __accessCheck = (obj, member, msg) => member.has(obj) || __typeError("Cannot " + msg);
+var __privateIn = (member, obj) => Object(obj) !== obj ? __typeError('Cannot use the "in" operator on this value') : member.has(obj);
+var __privateGet = (obj, member, getter) => (__accessCheck(obj, member, "read from private field"), getter ? getter.call(obj) : member.get(obj));
+var __privateSet = (obj, member, value, setter) => (__accessCheck(obj, member, "write to private field"), setter ? setter.call(obj, value) : member.set(obj, value), value);
+var __privateMethod = (obj, member, method) => (__accessCheck(obj, member, "access private method"), method);
 
 // lib/index.ts
 var index_exports = {};
 __export(index_exports, {
+  BaseAbstractRepository: () => BaseAbstractRepository,
+  DatabaseModule: () => DatabaseModule,
   Timestamp: () => Timestamp,
   Timestamp2: () => Timestamp2
 });
@@ -43,8 +90,53 @@ var Timestamp = class {
 };
 var Timestamp2 = class {
 };
+
+// lib/database/database.module.ts
+var import_common = require("@nestjs/common");
+var import_typeorm = require("@nestjs/typeorm");
+var _DatabaseModule_decorators, _init;
+_DatabaseModule_decorators = [(0, import_common.Module)({})];
+var DatabaseModule = class {
+  static forFeature(models) {
+    return import_typeorm.TypeOrmModule.forFeature(models);
+  }
+};
+_init = __decoratorStart(null);
+DatabaseModule = __decorateElement(_init, 0, "DatabaseModule", _DatabaseModule_decorators, DatabaseModule);
+__runInitializers(_init, 1, DatabaseModule);
+
+// lib/database/base.repository.ts
+var import_typeorm2 = require("typeorm");
+var BaseAbstractRepository = class extends import_typeorm2.Repository {
+  constructor(repo) {
+    super(repo.target, repo.manager);
+    this.repo = repo;
+  }
+  createAndSave(data) {
+    const object = this.repo.create(data);
+    return this.repo.save(object);
+  }
+  createManyAndSave(data) {
+    const object = this.repo.create(data);
+    return this.repo.save(object);
+  }
+  findOneByCondition(filterCondition) {
+    return this.repo.findOne(filterCondition);
+  }
+  findAll(options) {
+    return this.repo.find(options);
+  }
+  findWithRelations(relations) {
+    return this.repo.find(relations);
+  }
+  findOne(options) {
+    return this.repo.findOne(options);
+  }
+};
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
+  BaseAbstractRepository,
+  DatabaseModule,
   Timestamp,
   Timestamp2
 });
